06f40b5f22f56813767236c95b08a9f6f9edafec
kuz0lga <kuz0lga@yandex.ru>
2022-08-07 16:18:42 +0300

    add: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  
+  for( let i = 0; i < str.length; i++) {
+    let element = str[i];
+    let topElement = stack[stack.length - 1];
+    
+    for( let j = 0; j < bracketsConfig.length; j++) {
+      let open = bracketsConfig[j];
+
+      if(open.includes(element)) {
+        if (element == open[1]) { 
+          (topElement == open[0] ? stack.pop() : stack.push(element)) 
+        }
+          else {
+          stack.push(element);
+        }
+      }
+    }    
+  };
+
+  return stack.length === 0;
 }
